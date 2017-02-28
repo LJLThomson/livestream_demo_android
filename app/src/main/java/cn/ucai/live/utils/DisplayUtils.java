989c302645d5d4c36b648f03500f132a0b410633
LJLThomson <1114625580@qq.com>
2017-02-28 10:16:13 +0800

    添加工具类

---
+++ app/src/main/java/cn/ucai/live/utils/DisplayUtils.java
@@ -0,0 +1,21 @@
+package cn.ucai.live.utils;
+
+/**
+ * Created by Administrator on 2017/1/17 0017.
+ */
+
+public class DisplayUtils {
+//    public static void initBack(final Activity activity){
+//        activity.findViewById(R.id.backClickArea).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                activity.finish();
+//            }
+//        });
+//    }
+//    public static void initBackWithTitle(Activity activity, String title){
+//        TextView textView=(TextView) activity.findViewById(R.id.tv_common_Title);
+//        textView.setText(title);
+//        initBack(activity);
+//    }
+}
